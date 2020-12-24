8fbca8b5bc8b470dea1f9af0c49aaa1b0cd2a2a9
Emmanuel Dasilve <manuds941@gmail.com>
2020-12-24 11:11:55 +0100

    navbar fini + footer + controller

--- src/Controller/VitrineController.php
+++ src/Controller/VitrineController.php
@@ -9,11 +9,47 @@ use Symfony\Component\Routing\Annotation\Route;
 class VitrineController extends AbstractController
 {
     /**
-     * @Route("/", name="vitrine")
+     * @Route("/", name="accueil")
      */
     public function index(): Response
     {
         return $this->render('vitrine/index.html.twig', [
+            'controller_name' => 'VitrineController',
+        ]);
+    }
+    /**
+     * @Route("/histoire", name="histoire")
+     */
+    public function Histoire(): Response
+    {
+        return $this->render('vitrine/histoire.html.twig', [
+            'controller_name' => 'VitrineController',
+        ]);
+    }
+    /**
+     * @Route("/effectif", name="effectif")
+     */
+    public function Effectif(): Response
+    {
+        return $this->render('vitrine/effectif.html.twig', [
+            'controller_name' => 'VitrineController',
+        ]);
+    }
+    /**
+     * @Route("/media", name="media")
+     */
+    public function Media(): Response
+    {
+        return $this->render('vitrine/media.html.twig', [
+            'controller_name' => 'VitrineController',
+        ]);
+    }
+    /**
+     * @Route("/information", name="info")
+     */
+    public function Information(): Response
+    {
+        return $this->render('vitrine/info.html.twig', [
             'controller_name' => 'VitrineController',
         ]);
     }
