c7114c547ebd378058b0c441878fbe0af9b874e1
Emmanuel Dasilve <manuds941@gmail.com>
2021-01-04 08:48:46 +0100

    gestion admin + début remplissage dynamique

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,11 +2,15 @@
 
 namespace App\Controller;
 
+use DateTime;
 use App\Entity\User;
+use App\Entity\Accueil;
 use App\Entity\Message;
+use App\Form\AccueilType;
 use App\Form\InscriptionType;
-use App\Repository\MessageRepository;
 use App\Repository\UserRepository;
+use App\Repository\AccueilRepository;
+use App\Repository\MessageRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,41 +22,101 @@ class AdminController extends AbstractController
     /**
      * @Route("/admin", name="admin")
      */
-    public function index(Request $request): Response
+    public function index(Request $request, MessageRepository $repo): Response
     {
+        $message = $repo->findAll();
+
         return $this->render('admin/index.html.twig', [
             'controller_name' => 'AdminController',
-            'request' => $request
+            'request' => $request,
+            'message' => $message,
         ]);
     }
     /**
      * @Route("/admin/accueil", name="admin_accueil")
      */
-    public function GestionAccueil(Request $request): Response
+    public function GestionAccueil(Request $request, MessageRepository $repo, EntityManagerInterface $manager, AccueilRepository $repo1): Response
     {
+
+        $tableau = $manager->getClassMetadata(Accueil::class)->getFieldNames();
+
+        $message = $repo->findAll();
+        $accueil = $repo1->findAll();
+
+        dump($accueil);
+
         return $this->render('admin/gestion_accueil.html.twig', [
-            'controller_name' => 'AdminController',
-            'request' => $request
+            'request' => $request,
+            'message' => $message,
+            'tableau' => $tableau,
+            'accueil' => $accueil
         ]);
     }
+
+    /**
+     * @Route("/admin/accueil/create", name="admin_accueil_create")
+     */
+    public function AjoutAccueil(Accueil $accueil = null, Request $request, EntityManagerInterface $manager): Response
+    {
+        if(!$accueil)
+        {
+            $accueil = new Accueil;
+        }
+
+        $formAccueil = $this->createForm(AccueilType::class, $accueil);
+
+        $formAccueil->handleRequest($request);
+        
+        dump($request);
+
+        if( $formAccueil->isSubmitted() && $formAccueil->isValid())
+        {
+            if(!$accueil->getId())
+            {
+                $accueil->setCreatedAt(new \DateTime());
+            }
+            
+            $manager->persist($accueil);
+            $manager->flush();
+
+            $this->addFlash('success', "L'article a bien été ajouté !!");
+
+            return $this->redirectToRoute('admin_accueil', [
+                'id' => $accueil->getId() // On transmet le nouvel ID
+            ]);
+        }
+
+
+        return $this->render('admin/accueil_create.html.twig', [
+            'formAccueil' => $formAccueil->createView()
+        ]);
+    }
+
+
     /**
      * @Route("/admin/medias", name="admin_medias")
      */
-    public function GestionMédias(Request $request): Response
+    public function GestionMédias(Request $request, MessageRepository $repo): Response
     {
+        $message = $repo->findAll();
+
         return $this->render('admin/gestion_medias.html.twig', [
             'controller_name' => 'AdminController',
-            'request' => $request
+            'request' => $request,
+            'message' => $message,
         ]);
     }
     /**
      * @Route("/admin/info", name="admin_info")
      */
-    public function GestionInformations(Request $request): Response
+    public function GestionInformations(Request $request, MessageRepository $repo): Response
     {
+        $message = $repo->findAll();
+
         return $this->render('admin/gestion_info.html.twig', [
             'controller_name' => 'AdminController',
-            'request' => $request
+            'request' => $request,
+            'message' => $message,
         ]);
     }
     /**
@@ -72,10 +136,26 @@ class AdminController extends AbstractController
             'request' => $request
         ]);
     }
+
+    /**
+     * @Route("/admin/message/delete/{id}", name="admin_message_delete")
+     */
+    public function DeleteMessage(Message $message, EntityManagerInterface $manager)
+    {
+        $manager->remove($message);
+        $manager->flush();
+
+        $this->addFlash('success', "Le message a bien été supprimé");
+
+        $this->redirectToRoute('admin_message');
+    }
+
+
+
     /**
      * @Route("/admin/user", name="admin_user")
      */
-    public function GestionUser(EntityManagerInterface $manager, UserRepository $repo, Request $request): Response
+    public function GestionUser(EntityManagerInterface $manager, UserRepository $repo, Request $request, MessageRepository $repo1): Response
     {
         $tableau = $manager->getClassMetadata(User::class)->getFieldNames(); // Métas données User
 
@@ -85,10 +165,13 @@ class AdminController extends AbstractController
 
         dump($user);
 
+        $message = $repo1->findAll(); // Selectionne tout les messages pour voir si il y a un nouveau message sur le menu admin
+
         return $this->render('admin/gestion_user.html.twig', [
             'tableau' => $tableau,
             'user' => $user,
-            'request' => $request
+            'request' => $request,
+            'message' => $message,
         ]);
     }
 
@@ -132,5 +215,18 @@ class AdminController extends AbstractController
         return $this->render('admin/user_create.html.twig', [
             'formUser' => $formUser->createView(), // on créer une vision dans la variable formUser
         ]);
+    }
+
+    /**
+     * @Route("/admin/user/delete/{id}", name="admin_user_delete")
+     */
+    public function DeleteUser(User $user, EntityManagerInterface $manager)
+    {
+        $manager->remove($user);
+        $manager->flush();
+
+        $this->addFlash('success', "L'utilisateur a bien été supprimé");
+
+        $this->redirectToRoute('admin_user');
     }
 }
